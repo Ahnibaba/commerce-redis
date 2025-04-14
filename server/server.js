2f548d6c0ec35d57714285e9e6bc97176a94253c
Aniebiet Edet <Ahnibaba>
2025-04-14 17:39:29 +0100

    client2

--- server/server.js
+++ server/server.js
@@ -8,12 +8,14 @@ import couponRoutes from "./routes/coupon.route.js"
 import paymentRoutes from "./routes/payment.route.js"
 import analyticsRoutes from "./routes/analytics.route.js"
 import { connectDB } from "./lib/db.js"
+import path from "path"
 
 dotenv.config()
 
 const app = express()
-
 const PORT = process.env.PORT || 5000
+
+const __dirname = path.resolve()
 
 app.use(express.json({ limit: "10mb" })) //allows you to parse the body of the request
 app.use(cookieParser())
@@ -24,6 +26,15 @@ app.use("/api/cart", cartRoutes)
 app.use("/api/coupons", couponRoutes)
 app.use("/api/payment", paymentRoutes)
 app.use("/api/analytics", analyticsRoutes)
+
+
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/dist")))
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"))
+  })
+}
 
 app.listen(5000, () => {
   console.log(`Server is running on port ${PORT}`); 
