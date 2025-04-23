d920329920e9c5dd5f5b7ecd0eb2999443d7d2be
Aniebiet Edet <Ahnibaba>
2025-04-23 16:20:02 +0100

    cookies

--- client/vite.config.js
+++ client/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   server:{
     proxy:{
       "/api": {
-        target: "https://commerce-redis.onrender.com"
+        target: "https://commerce-redis.onrender.com/"
       }
     }
   }
